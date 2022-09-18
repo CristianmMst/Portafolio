65f92682786d108ad7e61e7fa9de6b11aa770d2c
Cristian Mora <cristianmmst@gmail.com>
2022-09-17 23:00:14 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-react", { runtime: "automatic" }]],
+        },
+      },
+      {
+        test: /.(css|sass|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        type: "asset",
+        test: /\.(png|svg|jpg|jpeg|gif$)/i,
+      },
+    ],
+  },
+};
