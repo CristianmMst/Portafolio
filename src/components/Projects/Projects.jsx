aedbc0441a4038ac213a55dfcbf38511720edcdc
Cristian Mora <cristianmmst@gmail.com>
2022-09-23 19:22:34 -0500

    section Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,22 +1,88 @@
 import styles from "./Projects.module.scss";
+import appSalud from "../../assets/appSalud.svg";
+import dogsApp from "../../assets/dogsApp.png";
+import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
 
 const About = () => {
   return (
-    <div id="projects" className={styles.about}>
-      <h1>about</h1>
-      <p>
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
-        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
-        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
-        Lorem duis laboris cupidatat officia voluptate. Culpa proident
-        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
-        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
-        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
-        et culpa duis.
-      </p>
+    <div id="projects" className={styles.projects}>
+      <Fade bottom duration={2000}>
+        <div className={styles.tittle}>
+          <div>
+            <h1>Proyectos</h1>
+            <div className={styles.subline}></div>
+          </div>
+        </div>
+      </Fade>
+      <div className={styles.containerCard}>
+        <div className={styles.card}>
+          <div className={styles.cardItem}>
+            <Fade left duration={2000}>
+              <h1>Más Salud</h1>
+              <p>
+                App para la gestión de Historias Clinica que incluya además de
+                búsquedas, filtros y ordenamientos, 3 tipos de usuario: User
+                paciente, User medico - User admin Integración pasarela (Mercado
+                Pago), auth0, notificaciones por email, administracion de
+                usuários, dashboard, Metodología SCRUM, Tecnologías: Lenguaje:
+                JavaScript, Base de datos: PostgreSQL - Sequelize- Backend: Node
+                Js - Express- Frontend: React - Redux. Estilos: CSS, Boostrap
+              </p>
+            </Fade>
+            <div style={{ marginTop: "2rem" }}>
+              <a
+                style={{ marginRight: "10px" }}
+                href="https://app-clinica.vercel.app"
+                target="_blank"
+                className={styles.btn}
+              >
+                ver pagina
+              </a>
+              <a
+                href="https://github.com/Ivan18Gauna/app-clinica"
+                target="_blank"
+                className={styles.btn}
+              >
+                ver Codigo
+              </a>
+            </div>
+          </div>
+          <Fade right duration={2000}>
+            <img src={appSalud} alt="appSalud" />
+          </Fade>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardItem}>
+            <Fade left duration={2000}>
+              <h1>Dogs-App</h1>
+              <p>
+                Diseñar y desarrollar una App de perros que incluía: búsquedas,
+                filtrados, ordenamientos y creación.Desarrollar la app usando
+                para el Front React, Redux, CSS puro y Back desarrollado en
+                Node.js con Express. Base de datos en PostgreSQL y Sequelize.
+                Deploy realizado con Heroku y Vercel.
+              </p>
+            </Fade>
+            <div style={{ marginTop: "2rem" }}>
+              <a
+                style={{ marginRight: "10px" }}
+                href="https://client-dogs.vercel.app"
+                target="_blank"
+                className={styles.btn}
+              >
+                ver pagina
+              </a>
+              <a href="#" target="_blank" className={styles.btn}>
+                ver Codigo
+              </a>
+            </div>
+          </div>
+          <Fade right duration={2000}>
+            <img src={dogsApp} alt="dogsApp" />
+          </Fade>
+        </div>
+      </div>
     </div>
   );
 };
