8eb4234fb2328e4d33eaf4c22b7dd0b01686484d
Cristian Mora <cristianmmst@gmail.com>
2022-09-24 22:08:10 -0500

    Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,16 @@
 import { Element } from "react-scroll";
+import "./Contact.scss";
 
-const Contact = ({ contact }) => {
-  return <Element name="contact">Contact</Element>;
+const Contact = () => {
+  return (
+    <Element name="contact" className="contact">
+      <h1>Contactame</h1>
+      <p>
+        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+        cillum sint consectetur cupidatat.
+      </p>
+    </Element>
+  );
 };
 
 export default Contact;
